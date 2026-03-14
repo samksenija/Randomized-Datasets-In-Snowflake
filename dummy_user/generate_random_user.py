94a6974ddd1428117eea292bc378c93070a7c641
ksenijasam <ksenija2702@gmail.com>
2026-03-14 19:41:26 +0100

    generate random user - init commit

---
+++ dummy_user/generate_random_user.py
@@ -0,0 +1,18 @@
+#Random name and surname generator using the Random User API
+import requests
+
+def generate_random_name_and_surname(number_of_records):
+    try:
+        response = requests.get(f"https://randomuser.me/api/?inc=name,email&results={number_of_records}")
+        data = response.json()
+
+        persons = []
+
+        for person in data['results']:
+            name = person['name']['first']
+            surname = person['name']['last']
+            email = person['email']
+
+            persons.append((name, surname, email))
+    except:
+        print('Something went wrong with radnomuser API, unfortunately. Please check.')
